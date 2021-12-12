269612b33d658072dcbc65adcd08cc2ee9e3d828
zubeyrberber <75991823+Zuby128@users.noreply.github.com>
2021-12-12 13:36:54 +0300

    Update Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,9 +5,9 @@ import { MainContainer, HomeImg, ImgDiv } from "./style";
 import Header from "../header/Header";
 import homeSvg from "../../assets/home.svg";
 
-// const APP_ID = process.env.REACT_APP_APP_ID;
-// const APP_KEY = process.env.REACT_APP_APP_KEY;
-// console.log(APP_ID, APP_KEY);
+const APP_ID = process.env.APP_ID;
+const APP_KEY = process.env.APP_KEY;
+
 
 const mealTypes = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
 
@@ -15,9 +15,6 @@ const Main = () => {
   const [query, setQuery] = useState("");
   const [recipes, setRecipes] = useState();
   const [meal, setMeal] = useState(mealTypes[0].toLowerCase());
-
-  const APP_ID = "4e9f05eb";
-  const APP_KEY = "9b904d703fa0d46a88ce1ac63f29f498";
 
   const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${meal}`;
 
